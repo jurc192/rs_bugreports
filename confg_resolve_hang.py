4f81be6d1187c25d93f6a2cb8ba39cf4f61f7b0b
jurc192 <vidmarjur@gmail.com>
2020-05-04 21:48:41 +0200

    Minimal example of config_resolve + log

--- confg_resolve_hang.py
+++ confg_resolve_hang.py
@@ -1,40 +1,18 @@
 
 import pyrealsense2 as rs
+from time import sleep
 
-def capture_depthframe(width=1280, height=720, exposure=0, laser_power=240, depth_preset=1):
+def capture_depthframe():
 
     pipeline = rs.pipeline()
     config   = rs.config()
-    config.enable_stream(rs.stream.depth, width, height)
-    if (config.can_resolve(pipeline) == False):
-        print("Resolution not supported")
-        return
-
-    print("Resolving configuration")
-    sensor = config.resolve(pipeline)   # hangs here
-    print("Resolved")
-    sensor = sensor.get_device().first_depth_sensor()
-
-    sensor.set_option(rs.option.depth_units, 0.0001)
-    sensor.set_option(rs.option.enable_auto_exposure, False)
-    sensor.set_option(rs.option.exposure, exposure)
-    sensor.set_option(rs.option.laser_power, laser_power)
-    sensor.set_option(rs.option.visual_preset, depth_preset)
-
+    config.enable_stream(rs.stream.depth)
+    print("\tResolving configuration....", end='')
+    profile = config.resolve(pipeline)   # hangs here after 200-ish iterations
+    print("OK")
     pipeline.start(config)
-    frame = pipeline.wait_for_frames().get_depth_frame()
-    config.disable_all_streams()
     pipeline.stop()
-    return frame
 
-
-if __name__ == "__main__":
-
-    resolutions = [(1280, 720), (848, 480), (640, 480), (640, 360), (480, 270)]
-    laserpowers = [150, 210, 240, 270, 300]
-    exposures   = [4500, 6500, 8500, 10500, 12500]
-
-    for res in resolutions:
-        for exp in exposures:
-            for lpow in laserpowers:
-                capture_depthframe(*res, exp, lpow, 1)
+for i in range(500):
+    print(i, end='')
+    capture_depthframe()
--- enablestream_bug.py
+++ enablestream_bug.py
@@ -1,12 +1,14 @@
 import pyrealsense2 as rs
 pipeline = rs.pipeline()
 config   = rs.config()
-# config.enable_stream(rs.stream.depth, 1, 848, 480, rs.format.z16, 15) # can't resolve
-# config.enable_stream(rs.stream.depth, 1)                              # can't resolve
+# config.enable_stream(rs.stream.depth, 0, 848, 480, rs.format.z16, 15) # works
+# config.enable_stream(rs.stream.depth, 0)                              # works
 # config.enable_stream(rs.stream.depth, rs.format.z16, 15)              # works
 # config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 15)    # USB2 fails, USB3 works
-# config.enable_stream(rs.stream.depth, 1, rs.format.z16, 15)           # can't resolve
+
+# config.enable_stream(rs.stream.depth, 0, rs.format.z16, 15)           # can't resolve
 
 # config.enable_stream(rs.stream.depth, 848, 480)                       # works
-# config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16)        # can't resolve
+config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16)        # can't resolve
 profile = config.resolve(pipeline)
+
